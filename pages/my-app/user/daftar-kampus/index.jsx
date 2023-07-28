45049d9f1281806beb8a17e3996bd5446f136fcc
has-bii <hasbii.rofficial@gmail.com>
2023-07-28 21:59:34 +0300

    Daftar Kampus

--- pages/my-app/user/daftar-kampus/index.jsx
+++ pages/my-app/user/daftar-kampus/index.jsx
@@ -2,14 +2,43 @@ import MyNavbar from "@components/MyNavbar";
 import UserDashboard from "@components/UserDashboard";
 import { fetchData } from "@lib/fetchData";
 import { fetchUser } from "@lib/fetchUser";
-import { hasCookie, setCookie } from "cookies-next";
+import axios from "axios";
+import { getCookie, hasCookie, setCookie } from "cookies-next";
 import Head from "next/head";
+import { useRef, useState } from "react";
 
-export default function index({ user, jurusans }) {
+export default function index({ user, jurusans, new_student }) {
+  const cookie = getCookie("user_token");
+  const [form, setForm] = useState({
+    jenis_kelamin: new_student.jenis_kelamin,
+    tanggal_lahir: new_student.tanggal_lahir,
+    provinsi_indonesia: new_student.provinsi_indonesia,
+    kota_asal_indonesia: new_student.kota_asal_indonesia,
+    alamat_lengkap_indonesia: new_student.alamat_lengkap_indonesia,
+    whatsapp: new_student.whatsapp,
+    no_paspor: new_student.no_paspor,
+    jenjang_pendidikan: new_student.jenjang_pendidikan,
+    jurusan_id: new_student.jurusan_id,
+  });
+
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    console.log(e.target.name.value);
+    const response = await axios
+      .post(`${process.env.NEXT_PUBLIC_API_URL}/api/newstudents/update`, form, {
+        headers: {
+          Authorization: `Bearer ${cookie}`,
+        },
+      })
+      .then((res) => {
+        return "Data submitted successfully";
+      })
+      .catch((err) => {
+        console.error(err);
+        return null;
+      });
+
+    console.log(response);
   };
 
   return (
@@ -36,7 +65,8 @@ export default function index({ user, jurusans }) {
                       placeholder="Nama lengkap"
                       id="nama"
                       name="name"
-                      required
+                      value={new_student?.name}
+                      readOnly
                     />
                     <label htmlFor="email">Email</label>
                     <input
@@ -44,10 +74,19 @@ export default function index({ user, jurusans }) {
                       placeholder="Alamat email"
                       id="email"
                       name="email"
-                      required
+                      value={new_student?.email}
+                      readOnly
                     />
                     <label htmlFor="kelamin">Jenis kelamin</label>
-                    <select required id="kelamin" name="jenis_kelamin">
+                    <select
+                      required
+                      id="kelamin"
+                      name="jenis_kelamin"
+                      value={form.jenis_kelamin}
+                      onChange={(e) =>
+                        setForm({ ...form, jenis_kelamin: e.target.value })
+                      }
+                    >
                       <option value="">Pilih jenis kelamin</option>
                       <option value="Laki-laki">Laki-laki</option>
                       <option value="Perempuan">Perempuan</option>
@@ -58,9 +97,11 @@ export default function index({ user, jurusans }) {
                       id="tanggal-lahir"
                       name="jenis_kelamin"
                       required
+                      value={form.tanggal_lahir}
+                      onChange={(e) =>
+                        setForm({ ...form, tanggal_lahir: e.target.value })
+                      }
                     />
-                  </div>
-                  <div className="_form_col">
                     <label htmlFor="whatsapp">Nomor Whatsapp</label>
                     <input
                       type="text"
@@ -68,6 +109,10 @@ export default function index({ user, jurusans }) {
                       name="whatsapp"
                       placeholder="Nomor Whatsapp dengan kode negara"
                       required
+                      value={form.whatsapp}
+                      onChange={(e) =>
+                        setForm({ ...form, whatsapp: e.target.value })
+                      }
                     />
                     <label htmlFor="no-paspor">Nomor Paspor</label>
                     <input
@@ -75,16 +120,39 @@ export default function index({ user, jurusans }) {
                       placeholder="Nomor paspor"
                       id="no-paspor"
                       name="no_paspor"
+                      required
+                      value={form.no_paspor}
+                      onChange={(e) =>
+                        setForm({ ...form, no_paspor: e.target.value })
+                      }
                     />
+                  </div>
+                  <div className="_form_col">
                     <label htmlFor="jenjang">Jenjang pendidikan</label>
-                    <select id="jenjang" name="jenjang_pendidikan" required>
+                    <select
+                      id="jenjang"
+                      name="jenjang_pendidikan"
+                      required
+                      value={form.jenjang_pendidikan}
+                      onChange={(e) =>
+                        setForm({ ...form, jenjang_pendidikan: e.target.value })
+                      }
+                    >
                       <option value="">Pendidikan yang akan ditempuh</option>
                       <option value="S1">S1</option>
                       <option value="S2">S2</option>
                       <option value="S3">S3</option>
                     </select>
                     <label htmlFor="jurusan">Jurusan</label>
-                    <select id="jurusan" name="jurusan_id" required>
+                    <select
+                      id="jurusan"
+                      name="jurusan_id"
+                      required
+                      value={form.jurusan_id}
+                      onChange={(e) =>
+                        setForm({ ...form, jurusan_id: e.target.value })
+                      }
+                    >
                       <option value="">Pilih jurusan</option>
                       {jurusans.map((j) => (
                         <option key={j.id} value={j.id}>
@@ -92,6 +160,49 @@ export default function index({ user, jurusans }) {
                         </option>
                       ))}
                     </select>
+                    <label htmlFor="provinsi-indonesia">Provinsi</label>
+                    <input
+                      type="text"
+                      id="provinsi-indonesia"
+                      name="provinsi_indonesia"
+                      placeholder="Masukan asal provinsi"
+                      required
+                      value={form.provinsi_indonesia}
+                      onChange={(e) =>
+                        setForm({ ...form, provinsi_indonesia: e.target.value })
+                      }
+                    />
+                    <label htmlFor="kota-asal">Kota</label>
+                    <input
+                      type="text"
+                      id="kota-asal"
+                      name="kota_indonesia"
+                      placeholder="Masukan asal kota"
+                      required
+                      value={form.kota_asal_indonesia}
+                      onChange={(e) =>
+                        setForm({
+                          ...form,
+                          kota_asal_indonesia: e.target.value,
+                        })
+                      }
+                    />
+                    <label htmlFor="alamat-lengkap">Alamat lengkap</label>
+                    <textarea
+                      id="alamat-lengkap"
+                      name="alamat_lengkap"
+                      placeholder="Masukan alamat lengkap"
+                      cols="30"
+                      rows="4"
+                      required
+                      value={form.alamat_lengkap_indonesia}
+                      onChange={(e) =>
+                        setForm({
+                          ...form,
+                          alamat_lengkap_indonesia: e.target.value,
+                        })
+                      }
+                    />
                   </div>
                 </div>
                 <button type="submit" className="ml-auto _green">
@@ -139,5 +250,9 @@ export async function getServerSideProps({ req, res, resolvedUrl }) {
       },
     };
 
-  return { props: { user, jurusans } };
+  const new_student = await fetchData("/newstudents", req, res, {
+    user_id: user.id,
+  });
+
+  return { props: { user, jurusans, new_student } };
 }
