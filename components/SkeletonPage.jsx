44f12ab70b0c53c37e483488990c1282514d6d87
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2023-08-14 15:35:10 +0300

    add container to SkeletonPage

--- components/SkeletonPage.jsx
+++ components/SkeletonPage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function SkeletonPage() {
   return (
-    <>
+    <div className="flex flex-col h-full gap-2">
       <div className="w-full h-8 rounded-md bg-slate-300 animate-pulse"></div>
-      <div className="w-full h-full rounded-md bg-slate-300 animate-pulse"></div>
-    </>
+      <div className="w-full h-full min-h-[8rem] rounded-md bg-slate-300 animate-pulse"></div>
+    </div>
   );
 }
